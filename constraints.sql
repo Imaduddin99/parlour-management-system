44e6db4012aa77da7dca2e03745fb8278a4eb82c
Imaduddin99 <168505427+Imaduddin99@users.noreply.github.com>
2026-06-18 21:56:42 -0500

    Delete constraints.sql

--- constraints.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE appointments
-ADD CONSTRAINT fk_customer
-FOREIGN KEY (customer_id)
-REFERENCES customers(customer_id);
-
-ALTER TABLE appointments
-ADD CONSTRAINT fk_staff
-FOREIGN KEY (staff_id)
-REFERENCES staff(staff_id);
